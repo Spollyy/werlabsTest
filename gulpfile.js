9ca46531a239ac5060c92b59fd8188a928a3fc56
Alex Filonenko <alexthefil@gmail.com>
2018-07-17 16:01:09 +0300

    Let's try github

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Load plugins
+var gulp            = require('gulp');
+var autoprefixer    = require('gulp-autoprefixer');
+var sass            = require('gulp-sass');
+var sourcemaps      = require('gulp-sourcemaps');
+
+
+// SCSS
+gulp.task('scss', function () {
+    gulp.src('scss/werlabs.scss')
+        .pipe(sourcemaps.init())
+
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('css'))
+});
+
+
+// Watch
+gulp.task('default', function() {
+    gulp.watch(['scss/werlabs.scss'], ['scss']);
+});
